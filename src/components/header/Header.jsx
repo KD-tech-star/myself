d9a11ee62837e092c0751bb61d1c10a6e381c57f
sonu kd <hiitechz07@gmail.com>
2022-12-23 14:53:43 +0530

    commited by me

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import CTA from './CTA'
-import "./header.css"
-import ME from "../../assets/me.png"
-import { HeaderSocial } from './HeaderSocial'
+import React from "react";
+import CTA from "./CTA";
+import "./header.css";
+import ME from "../../assets/me.png";
+import { HeaderSocial } from "./HeaderSocial";
 export default function Header() {
   return (
     <header>
-      <div className="container header__container"></div>
-      <h5>Hello I'm</h5>
-      <h1>Rozi</h1>
-      <h5 className="text-light">Fullstack Developer</h5>
-      <CTA/>
-      <HeaderSocial/>
+      <div className="container header__container">
+      <h5 style={{fontSize :"20px"}}>Hello I'm</h5>
+      <h1>Sonu Bharti</h1>
+      <h5 className="text-light">React Fullstack Developer</h5>
+      <CTA />
+      <HeaderSocial />
       <div className="me">
-        <img src={ME} alt="" />
+      <img src={ME} alt="me" />
       </div>
-
-      <a href="" className='scroll__down'>Scroll Down</a>
-
+      
+      <a href="#contact" className="scroll__down"> Scroll Down </a>
+      </div>
     </header>
-  )
+  );
 }
--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import {BsLinkedin} from "react-icons/bs"
 import {GoMarkGithub} from "react-icons/go"
 import {FaFacebookSquare} from "react-icons/fa"
+import {FiInstagram} from "react-icons/fi"
+
 
 
 export const HeaderSocial = () => {
   return (
     <div className='header__socials'>
     <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-    <a href="https://github.com" target="_blank"><GoMarkGithub/></a>
+    <a href="https://github.com/KD-tech-star" target="_blank"><GoMarkGithub/></a>
     <a href="https://facebook.com" target="_blank"><FaFacebookSquare/></a>
+    <a href=""><FiInstagram/></a>
 
-    
     
     
     </div>
